73cf2f588ff7947af1470463dd5a6f32b7580ad9
aggieKevin <41754728+aggieKevin@users.noreply.github.com>
2019-05-15 17:53:36 -0500

    second commit

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
-from . views import PostListView,PostDetailView
+from . views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+    )
 urlpatterns = [
     path('',PostListView.as_view(),name='blog_home'),
-    path('blog/<int:pk>/',PostDetailView.as_view(),name='blog_detail'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='post_detail'),
+    path('post/new',PostCreateView.as_view(),name='post_create'),
+    path('post/<int:pk>/update',PostUpdateView.as_view(),name='post_update'),
+    path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post_delete'),
     path('about/',views.about, name='blog_about')
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 def home(request):
     context={'posts':Post.objects.all()}
@@ -16,12 +16,48 @@ def about(request):
 
 class PostListView(ListView):
     model=Post
-    template='app_blog/home.html'
+    template_name='app_blog/home.html' 
+    # if note created, the view will use default template
+    #<app>/<model>_<viewtype>.html
     context_object_name='posts'
     ordering=['-date_posted']
 
 class PostDetailView(DetailView):
     model=Post
-    template='app_blog/blog_detail.html'
+    template_name='app_blog/blog_detail.html'
     context_object_name='post'
     
+class PostCreateView(LoginRequiredMixin,CreateView): # how to require log in for class view
+    model=Post
+    fields=['title','content']
+    template_name='app_blog/blog_create.html'
+    context_object_name='form'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): # how to require log in for class view
+    model=Post
+    fields=['title','content']
+    template_name='app_blog/blog_create.html'
+    context_object_name='form'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post
+    context_object_name='post' # the default name is object
+    template_name='app_blog/blog_delete.html'
+    success_url='/'
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        return False
